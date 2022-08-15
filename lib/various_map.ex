3fa4f21d40a5af89be000f44bf6e99f26ccfc24a
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-16 06:14:03 +0900

    initial commit

---
+++ lib/various_map.ex
@@ -0,0 +1,18 @@
+defmodule VariousMap do
+  @moduledoc """
+  Documentation for `VariousMap`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VariousMap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/various_map_test.exs
@@ -0,0 +1,8 @@
+defmodule VariousMapTest do
+  use ExUnit.Case
+  doctest VariousMap
+
+  test "greets the world" do
+    assert VariousMap.hello() == :world
+  end
+end
