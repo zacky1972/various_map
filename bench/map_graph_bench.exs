d3cc2378b400e3ac32656823b7ff15938379c4ae
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-16 17:02:38 +0900

    create map_graph_bench

---
+++ bench/map_graph_bench.exs
@@ -0,0 +1,56 @@
+import VariousMap.MapGraph
+
+defmodule RandomMapGraph do
+  def random_edges(v_size) do
+    for v1 <- 1..v_size, v2 <- 1..v_size do
+      {v1, v2, :rand.uniform(1000)}
+    end
+    |> Enum.shuffle()
+  end
+
+  def generate(edges, e_size) do
+    edges
+    |> Enum.take(e_size)
+    |> Enum.reduce(%{}, fn {v1, v2, value}, graph ->
+      put(graph, v1, v2, value)
+    end)
+  end
+
+  def max_edges(v_size) do
+    v_size * (v_size - 1)
+  end
+end
+
+:rand.seed(:exsss, {100, 101, 102})
+
+v_sizes = [10, 100, 1_000]
+random_edges = Enum.map(v_sizes, &RandomMapGraph.random_edges/1)
+
+inputs =
+  Enum.zip(v_sizes, random_edges)
+  |> Enum.map(fn {v_size, edges} ->
+    [v_size, v_size * 2, RandomMapGraph.max_edges(v_size)]
+    |> Enum.map(fn e_size ->
+      {
+        "Graph {num_vertex, num_edge} = {#{v_size}, #{e_size}}",
+        {
+          {v_size, e_size},
+          RandomMapGraph.generate(edges, e_size)
+        }
+      }
+    end)
+  end)
+  |> List.flatten()
+  |> Map.new()
+
+Benchee.run(
+  %{
+    "MapGraph.get" => fn {{v_size, _e_size}, input} ->
+      get(input, :rand.uniform(v_size), :rand.uniform(v_size))
+    end,
+    "MapGraph.put" => fn {{v_size, _e_size}, input} ->
+      put(input, :rand.uniform(v_size), :rand.uniform(v_size), :rand.uniform(1000))
+    end
+  },
+  inputs: inputs
+)
